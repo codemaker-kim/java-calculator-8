e4aafacc679e590733420988d5fd3f5f06d680fe
codemaker-kim <tnals72441@daum.net>
2025-10-15 18:49:46 +0900

    feat: 커스텀 구분자 로직 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -28,12 +28,22 @@ public class Application {
         }
 
         String regex = ",|:";
+        String customDelimiter = getCustomDelimiter(input);
+
+        if (customDelimiter != null) {
+            regex += "|" + customDelimiter;
+            input = getExpressionPart(input);
+        }
 
         String[] targets = input.split(regex);
 
         return Arrays.stream(targets)
                 .mapToInt(Application::isNegative)
                 .sum();
+    }
+
+    private static String getExpressionPart(String input) {
+        return input.substring(input.indexOf(CUSTOM_DELIMITER_SUFFIX) + CUSTOM_DELIMITER_SUFFIX.length());
     }
 
     private static int isNegative(String s) {
@@ -43,5 +53,14 @@ public class Application {
         }
 
         return num;
+    }
+
+    private static String getCustomDelimiter(final String delimiterPart) {
+        if (!delimiterPart.startsWith(CUSTOM_DELIMITER_PREFIX) || !delimiterPart.contains(CUSTOM_DELIMITER_SUFFIX)) {
+            return null;
+        }
+
+        return delimiterPart.substring(CUSTOM_DELIMITER_PREFIX.length(),
+                delimiterPart.lastIndexOf(CUSTOM_DELIMITER_SUFFIX));
     }
 }
