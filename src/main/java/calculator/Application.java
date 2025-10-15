a348a69a20522dcf2b979798a430bfd07557a954
codemaker-kim <tnals72441@daum.net>
2025-10-15 16:29:44 +0900

    feat: 테스트를 기반으로 한 기본 계산기 로직 작성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,38 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 public class Application {
+    private static final String CUSTOM_DELIMITER_PREFIX = "//";
+    private static final String CUSTOM_DELIMITER_SUFFIX = "\\n";
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input;
+        try {
+            input = Console.readLine();
+        } catch (NoSuchElementException e) {
+            input = "";
+        }
+
+        int result = calculate(input);
+        System.out.println("결과 : " + result);
+    }
+
+    // 객체 단위로 쪼개서 구현하기.
+    private static int calculate(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        String regex = ",|:";
+
+        String[] targets = input.split(regex);
+
+        return Arrays.stream(targets)
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 }
